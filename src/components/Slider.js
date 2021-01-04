90bbe27475a91e177c155555237fa57b07f84f06
Josh Bryson <josh@bryson.me.uk>
2021-01-04 00:14:49 +0000

    Minor styling updated

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import './Inputs.scss';
 
 const Slider = ({ min, max, value, handleChange}) => {
   return (
     <div className="slider-container">
+        <span>{value}%</span>
       <input
         type="range"
         className="slider"
